73d206879c6ecff626421ffaa718e8dc49e8485f
Narariye <bagassetyatama@gmail.com>
2025-11-11 06:37:48 +0700

    Add files via upload

---
+++ LED/sketch.ino
@@ -0,0 +1,31 @@
+#define LED1 1
+#define LED2 2
+#define LED3 42
+
+void TaskLED(void *pvParameters) {
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+
+  while (1) {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, HIGH);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, LOW);
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  // core 0
+  xTaskCreatePinnedToCore(TaskLED, "LED3", 2048, NULL, 1, NULL, 0);
+
+  // core 1
+  //xTaskCreatePinnedToCore(TaskLED, "LED3", 2048, NULL, 1, NULL, 1);  
+}
+
+void loop() {}
